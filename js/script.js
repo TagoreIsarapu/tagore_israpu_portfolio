ae80a93e1faa3caac1f476bb25c9b6054556207b
TagoreIsarapu <144801938+TagoreIsarapu@users.noreply.github.com>
2026-07-27 22:54:21 +0530

    Implement section reveal on scroll
    Add scroll event listener to reveal sections

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("Portfolio Loaded Successfully");
+const sections = document.querySelectorAll("section");
+
+
+window.addEventListener("scroll",()=>{
+
+    sections.forEach(section=>{
+
+        let position = window.scrollY;
+
+        let offset = section.offsetTop - 500;
+
+
+        if(position > offset){
+
+            section.classList.add("show");
+
+        }
+
+    });
+
+});
