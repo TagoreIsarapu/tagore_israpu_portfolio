962c1d5eac60b9621780b6e5c8b660266dfe711c
TagoreIsarapu <144801938+TagoreIsarapu@users.noreply.github.com>
2026-07-29 16:59:59 +0530

    Refactor scroll event listener for sections

--- js/script.js
+++ js/script.js
@@ -1,22 +1,14 @@
 console.log("Portfolio Loaded Successfully");
+
 const sections = document.querySelectorAll("section");
 
-
-window.addEventListener("scroll",()=>{
-
-    sections.forEach(section=>{
-
+window.addEventListener("scroll", () => {
+    sections.forEach(section => {
         let position = window.scrollY;
-
         let offset = section.offsetTop - 500;
 
-
-        if(position > offset){
-
+        if (position > offset) {
             section.classList.add("show");
-
         }
-
     });
-
 });
